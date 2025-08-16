8d5541fa99fb30108a715e904809c19957dfa18d
jovan <jovanus@yahoo.co.id>
2025-08-17 01:37:16 +0700

    feat: sidebar

---
+++ packages/web/src/shared/components/ui/separator/Separator.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { SeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { Separator } from 'reka-ui'
+import { cn } from '@/shared/utils'
+
+const { class: className, ...props } = withDefaults(
+  defineProps<SeparatorProps & { class?: HTMLAttributes['class'] }>(),
+  {
+    orientation: 'horizontal',
+    decorative: true,
+  },
+)
+</script>
+
+<template>
+  <Separator
+    data-slot="separator-root"
+    v-bind="props"
+    :class="
+      cn(
+        `bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px`,
+        className,
+      )
+    "
+  />
+</template>
